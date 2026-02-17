18ca7aa1bebd455872e1975425412aff777acee5
sinchan27 <gsinchan27@gmail.com>
2026-02-17 18:35:50 +0530

    fix vercel deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/invoice-generator/",
   plugins: [react(), tailwindcss()],
+  base: "./"
 })
